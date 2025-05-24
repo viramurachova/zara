056d40c3fdd919edd90d2a7cc5e402a67b8df6b6
vmurachova <v.murachova@temper.works>
2025-05-24 16:33:03 +0200

    fix the coockie

--- src/fixtures/cookieFixture.ts
+++ src/fixtures/cookieFixture.ts
@@ -61,7 +61,7 @@
 //   pageWithCookies: async ({ page }, use) => {
 //     await page.goto('/');
 //     const acceptCookiesButton = page.locator('#onetrust-accept-btn-handler');
-//     const goToStoreButton = page.locator('[data-qa-action="go-to-store"]');
+//     const goToStoreButton = page.locator('[data-qa-action="stay-in-store"]');
 
 //     await acceptCookiesButton.waitFor({ state: 'visible' });
 //     await acceptCookiesButton.click();
@@ -74,44 +74,26 @@
 // });
 
 
-import { test as base, expect, Page, Browser, BrowserContext } from '@playwright/test';
-import { chromium as extraChromium } from 'playwright-extra';
-import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+// 
 
-extraChromium.use(StealthPlugin());
+import { test as base, Page } from '@playwright/test';
 
 export const test = base.extend<{
   pageWithCookies: Page;
 }>({
-  pageWithCookies: async ({}, use) => {
-    const browser: Browser = await extraChromium.launch({ headless: true });
-
-    const context: BrowserContext = await browser.newContext({
-      locale: 'uk-UA',
-      geolocation: { latitude: 50.4501, longitude: 30.5234 }, 
-      permissions: ['geolocation'],
-      viewport: { width: 1280, height: 720 },
-    });
-
-    const page: Page = await context.newPage();
-
-    await page.setExtraHTTPHeaders({
-      'Accept-Language': 'en-US,en;q=0.9'
-    });
-
+  pageWithCookies: async ({ page }, use) => {
     await page.goto('https://www.zara.com/ua/en/');
 
     const acceptCookiesButton = page.locator('#onetrust-accept-btn-handler');
-    // 
-    const goToStoreButton = page.locator('[data-qa-action="stay-in-store"]');
+    if (await acceptCookiesButton.isVisible({ timeout: 5000 })) {
+      await acceptCookiesButton.click();
+    }
 
-    await acceptCookiesButton.waitFor({ state: 'visible' });
-    await acceptCookiesButton.click();
-
-    await goToStoreButton.waitFor({ state: 'visible' });
-    await goToStoreButton.click();
+    const continueUkraineButton = page.locator('[data-qa-action="stay-in-store"]');
+    if (await continueUkraineButton.isVisible({ timeout: 5000 })) {
+      await continueUkraineButton.click();
+    }
 
     await use(page);
-    
   }
 });
