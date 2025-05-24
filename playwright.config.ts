a393af2221d8adc714559578c5bed3d6ce96e4ae
vmurachova <v.murachova@temper.works>
2025-05-24 15:00:32 +0200

    update Playwright yml file

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,8 +12,10 @@ export default defineConfig({
     baseURL: 'https://www.zara.com/',
     viewport: { width: 1280, height: 720 },
     userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_0_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-    headless: false,
-    trace: 'on',          
+    headless: true,
+    trace: 'on',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',  
   },
   projects: [
     {
